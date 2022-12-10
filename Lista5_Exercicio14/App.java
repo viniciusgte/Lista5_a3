f4ca8905181d314213d6656fcf70a361835b4d0c
Vinícius Tachetti <114702851+viniciusgte@users.noreply.github.com>
2022-12-10 13:12:40 -0300

    Atividade 14
    Quadrado mágico. Um quadrado mágico é aquele dividido em linhas e colunas, com um número em cada posição e no qual a soma das linhas, colunas e diagonais é a mesma. Por exemplo, veja um quadrado mágico de lado 3, com números de 1 a 9:
    
    8  3  4 
    1  5  9
    6  7  2
    Elabore uma função que identifica e mostra na tela todos os quadrados mágicos com as características acima. Dica: produza todas as combinações possíveis e verifique a soma quando completar cada quadrado. Usar um vetor de 1 a 9 parece ser mais simples que usar uma matriz 3x3.

---
+++ Lista5_Exercicio14/App.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    
+        ArrayList<Integer> quadrado = GeradorQuadradoRandom.gerar_Quadrado();
+
+        int x1 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x2 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x3 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x4 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x5 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x6 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x7 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x8 = GeradorQuadradoRandom.NumAleatorio(quadrado),
+            x9 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+
+        int contador = 1;
+
+        while(
+            ((x1 + x2 + x3) != 15) || ((x4 + x5 + x6) != 15) || ((x7 + x8 + x9) != 15) ||
+            ((x1 + x4 + x7) != 15) || ((x2 + x5 + x8) != 15) || ((x3 + x6 + x9) != 15) ||
+            ((x1 + x5 + x9) != 15) || ((x3 + x5 + x7) != 15)
+        ) {
+            quadrado = GeradorQuadradoRandom.gerar_Quadrado();
+            contador++;
+            x1 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x2 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x3 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x4 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x5 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x6 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x7 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x8 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+            x9 = GeradorQuadradoRandom.NumAleatorio(quadrado);
+        }
+
+        
+
+        System.out.println(x1 + " - " + x2 + " - " + x3);
+        System.out.println(x4 + " - " + x5 + " - " + x6);
+        System.out.println(x7 + " - " + x8 + " - " + x9);
+
+        System.out.println(" ");
+        System.out.println("Ao lado, temos o número de tentativas: " + contador);
+
+    }
+
+}
